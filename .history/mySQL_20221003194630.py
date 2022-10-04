1109e9327d9889e115866821048c37fc7def4da4
csanicola74 <caroline.sanicola@stonybrook.edu>
2022-10-03 20:06:11 -0400

    initial

---
+++ .history/mySQL_20221003194630.py
@@ -0,0 +1,20 @@
+# CONNECTING TO MYSQL WITH GOOGLE COLLAB - PYTHON FRAMEWORK
+
+#######################
+##  INSTALL PACKAGES ##
+#######################
+
+# !sudo apt-get install python3-dev default-libmysqlclient-dev
+# !pip3 install pymysql
+
+from sqlalchemy import create_engine
+
+MYSQL_HOSTNAME = 'INSERT_HERE'
+MYSQL_USER = 'INSERT_HERE'
+MYSQL_PASSWORD = 'INSERT_HERE'
+MYSQL_DATABASE = 'INSERT_HERE'
+
+connection_string = f'mysql+pymysql://{MYSQL_USER'
+
+
+https: // www.ers.usda.gov/webdocs/DataFiles/50048/Feed % 20Grains % 20Yearbook % 20Tables-All % 20Years.xlsx?v = 5831.9
